4dcf3e7caefb463843ab1640bf1f76071610f2fe
MahmoudNasser1242000 <mahmoudahmed102030405060@gmail.com>
2024-08-19 19:17:00 +0300

    get-specific-subCategory

--- src/modules/subCategory/subCategory.controller.js
+++ src/modules/subCategory/subCategory.controller.js
@@ -1,6 +1,7 @@
 import slugify from "slugify";
 import errorAsyncHandler from "../../../services/errorAsyncHandler.js";
 import subCategoryModel from "../../../database/models/subCategory.model.js";
+import AppError from "../../../utils/errorClass.js";
 
 const addSubCategorey = errorAsyncHandler(async (req, res, next) => {
     const {categoryId} = req.params
@@ -14,7 +15,15 @@ const getAllSubCategories = errorAsyncHandler(async (req, res, next) => {
     res.status(200).json({subCategories});
 })
 
+const getSpecificSubCategory = errorAsyncHandler(async (req, res, next) => {
+    const subCategory = await subCategoryModel.findById(req.params.subCategoryId);
+    if (!subCategory) 
+        return next(new AppError("Cant not find subCategory with this id", 400))
+    res.status(200).json({subCategory});
+})
+
 export {
     addSubCategorey,
-    getAllSubCategories
+    getAllSubCategories,
+    getSpecificSubCategory,
 }
--- src/modules/subCategory/subCategory.routes.js
+++ src/modules/subCategory/subCategory.routes.js
@@ -1,15 +1,16 @@
 import {Router} from "express";
-import { addSubCategorey, getAllSubCategories } from "./subCategory.controller.js";
+import { addSubCategorey, getAllSubCategories, getSpecificSubCategory } from "./subCategory.controller.js";
 import checkCategoryId from "../../middlewares/checkCategoryId.js";
 
 const subCategoryRouter = Router();
 
 subCategoryRouter.get("/", getAllSubCategories)
 
-subCategoryRouter.route("/:categoryId")
-    .post(
-        checkCategoryId,
-        addSubCategorey
+subCategoryRouter.post("/:categoryId", checkCategoryId, addSubCategorey)
+
+subCategoryRouter.route("/:subCategoryId")
+    .get(
+        getSpecificSubCategory
     )
 
 export default subCategoryRouter;
